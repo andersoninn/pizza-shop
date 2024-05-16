6b93595f05c6a83d5fbaeac1eca6cf9366859bbb
unknown <andersoninnocencio17@gmail.com>
2024-05-16 16:51:54 +0100

    style: layout Auth Page

---
+++ src/app/Auth/page.tsx
@@ -0,0 +1,31 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+export default function page() {
+   return (
+      <>
+         <div className="p-8">
+            <div className="w-[350px] flex flex-col justify-center gap-6">
+               <div className="flex flex-col gap-2 text-center">
+                  <h1 className="text-2xl font-semibold tracking-tight">
+                     Acessar painel
+                  </h1>
+                  <p className="text-sm text-muted-foreground">
+                     Acompanhe suas vendas pelo painel do parceiro!
+                  </p>
+                  <form className="space-y-4">
+                     <div className="space-y-2">
+                        <Label htmlFor="email">Seu e-mail</Label>
+                        <Input id="email" type="emaail" />
+                     </div>
+                     <Button className="w-full" type="submit">
+                        Acessar painel
+                     </Button>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </>
+   );
+}
